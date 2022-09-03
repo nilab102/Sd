daf8e923fa1759f60da9bb6720dcf7ac3fb89e63
nilab102 <nilab102@gmail.com>
2022-09-04 01:29:16 +0600

    09/04

--- AskMe/Controllers/CategoryController.cs
+++ AskMe/Controllers/CategoryController.cs
@@ -10,49 +10,8 @@ namespace AskMe.Controllers
     public class CategoryController : Controller
     {// GET: Category
         AskMeEntities dbobj = new AskMeEntities();
-        public ActionResult AddCategory()
-        {
-           return View();
-        }
-
-        [HttpPost]
-        public ActionResult AddCategory(Category model)
-        {
-            Category obj = new Category();
-            if (dbobj.Categories.Any(x => x.CategoryName == model.CategoryName))
-            {
-                ViewBag.notification = "This Category has already existed";
-                return View();
-            }
-            if (!ModelState.IsValid)
-            {
-                obj.CategoryId = model.CategoryId;
-                obj.CategoryName = model.CategoryName;
-
-                if (model.CategoryId == 0)
-                {
-                    dbobj.Categories.Add(obj);
-                    dbobj.SaveChanges();
-                }
-                else
-                {
-                    dbobj.Entry(obj).State = System.Data.Entity.EntityState.Modified;
-                    dbobj.SaveChanges();
-                }
-
-            }
-            ModelState.Clear();
-
-           // var list = dbobj.Categories.ToList();
-            return View();
-        }
-
-        public ActionResult CategoryList()
-        {
-            var res = dbobj.Categories.ToList();
-            return View(res);
-        }
-
+   
+       
         public ActionResult Delete(int CategoryId)
         {
            
